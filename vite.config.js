50eba92461bc2cc6397061042b9572665e160b7a
cpmas <calemason11@gmail.com>
2026-04-28 19:47:14 +1000

    upload

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/<thai-learning-app>/', // Don't forget to replace this with your GitHub repo name!
+})
